bbcc1e832bf727bb2b146121fa2a6895e8135253
wulin <59204880+54wulin@users.noreply.github.com>
2023-05-15 17:59:09 +0800

    asdfsaf

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -34,7 +34,9 @@ public class CategoryController {
     public R<String> save( @RequestBody Category category){
         log.info("category:{}",category);
         categoryService.save(category);
+        String a = "";
         return  R.success("新增分类成功");
+
     }
 
 
